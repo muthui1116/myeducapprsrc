fae349b838305fe7f345d117eebc9e30cdb2b7ad
nzeluni girls <muthuikyalozis1@gmail.com>
2021-09-16 21:18:48 +0300

    initial commit

--- edublog/models.py
+++ edublog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=255)
+    content = RichTextField(null=True, blank=True)
+    # content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- educpy/settings.py
+++ educpy/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'edublog.apps.EdublogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
